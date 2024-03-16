121f5fca6d09032b83da268753c23e519ba2ad63
凯撒 <1290361724@qq.com>
2024-03-16 06:44:17 +0000

    update 1.bat.
    Signed-off-by: 凯撒 <1290361724@qq.com>

--- 1.bat
+++ 1.bat
@@ -1,15 +1,8 @@
-color a
-eho 123131232
-color b
-eho 123131232
-color 1 
-eho 123131232
-color 2 
-eho 123131232
-color 3
-eho 123131232
-color 4 
-eho 123131232
-color 4
-eho 123131232
-%0
+@echo off
+set name=%~n0
+echo 这是%name%脚本
+echo 111开始111
+echo %date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%-%time:~3,2%-%time:~6,2%
+ping 127.0.0.1
+echo 111结束111
+echo %date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%-%time:~3,2%-%time:~6,2%
