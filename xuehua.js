156ec76be11757156695176be0ee70fd111e8376
kaysarsoft <1290361724@qq.com>
2025-08-08 16:50:46 +0800

    更新雪花特效代码

--- xuehua.js
+++ xuehua.js
@@ -1,1 +1,142 @@
-;var _0xodF='jsjiami.com.v6',_0xodF_=['_0xodF'],_0x5bce=[_0xodF,'w5nDgcOOcV3DocKhdcKww4Bow5ts','RRhJdiBK','wrRHDsK1b8O9WXhqw77CncKT','XsOrw7BGFg==','wpXDgCbDrglQaQvDhknDrMOQ','VV/CrQ==','NCZeeMKT','wpDDq19c','L8OBOcOMwpFIwq14wplQw50=','w6Udw4XCkirDhQ==','Uh0SFHLDuw==','woEdwodFRg==','b8OFDV/DgMOh','Uwkg','VQcKC2DCpcO3YMO4wrLDq3FLFcOvw4BfYlbCrcOEFyLDsMKOw5nCuTNoBcKNZcKjwoQPwpnCk8Kbw4YzJTbDkcO+eApIOQ==','VQlefiRbw718SMOmw57Ciik=','wpLCqAXDlsKu','wo/DizzDig9sTy/DqA==','TjNvdMK3b8KFFsOZE3TCnHhCPsKIGADClUXDnlUyw4HCgT8mwrwJCMKbwqcwL8OFwr5wfVIlwol9GcKjwpokw4N5w6xIKh/CrcK3wpdCPMKRWcOVYGVYw7wcwp1Vw6HDmMOCesKbLCDDjsK5wp7CiG/Dg8K5wotVw4VywpXCu8K5wqUAwoUJwrbCs8OGLsO/OsOjwq7Cv8KxSGQCwolDOFLCtMKwKsOKw79Iw4vDhE7CuygrGDnCtsK0IMOHwqTCpnXDu8OuWMKpU8OUw4HDosKrw6Jyw4ILw7ozw5cxwopbwqtAd2opBw==','w4/Dlnsx','woE8w64YDhfDvl1dw7h9','cFHCqljDm23Dqw0Lw7Fowqo4w5pnHCA=','jVsDQSjiawhmfMLiRA.com.v6=='];if(function(_0x55822b,_0x44e891,_0x5c18aa){function _0x52be0a(_0x1cedcf,_0x3d34b9,_0x487b6f,_0x446a43,_0x4de1ab,_0x495da3){_0x3d34b9=_0x3d34b9>>0x8,_0x4de1ab='po';var _0x3e6d3c='shift',_0x45ca4a='push',_0x495da3='0.fs21mzwm19';if(_0x3d34b9<_0x1cedcf){while(--_0x1cedcf){_0x446a43=_0x55822b[_0x3e6d3c]();if(_0x3d34b9===_0x1cedcf&&_0x495da3==='0.fs21mzwm19'&&_0x495da3['length']===0xc){_0x3d34b9=_0x446a43,_0x487b6f=_0x55822b[_0x4de1ab+'p']();}else if(_0x3d34b9&&_0x487b6f['replace'](/[VDQSwhfMLRA=]/g,'')===_0x3d34b9){_0x55822b[_0x45ca4a](_0x446a43);}}_0x55822b[_0x45ca4a](_0x55822b[_0x3e6d3c]());}return 0x10f6dd;};return _0x52be0a(++_0x44e891,_0x5c18aa)>>_0x44e891^_0x5c18aa;}(_0x5bce,0x91,0x9100),_0x5bce){_0xodF_=_0x5bce['length']^0x91;};function _0x14c9(_0x3acacf,_0x2f3c8){_0x3acacf=~~'0x'['concat'](_0x3acacf['slice'](0x0));var _0x2093ae=_0x5bce[_0x3acacf];if(_0x14c9['qOIxZO']===undefined){(function(){var _0x4e4239=typeof window!=='undefined'?window:typeof process==='object'&&typeof require==='function'&&typeof global==='object'?global:this;var _0x25f8fd='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x4e4239['atob']||(_0x4e4239['atob']=function(_0x5e2c9e){var _0x21569d=String(_0x5e2c9e)['replace'](/=+$/,'');for(var _0x4edb03=0x0,_0x4fa6d9,_0x34e0ce,_0x1027d2=0x0,_0x1d705='';_0x34e0ce=_0x21569d['charAt'](_0x1027d2++);~_0x34e0ce&&(_0x4fa6d9=_0x4edb03%0x4?_0x4fa6d9*0x40+_0x34e0ce:_0x34e0ce,_0x4edb03++%0x4)?_0x1d705+=String['fromCharCode'](0xff&_0x4fa6d9>>(-0x2*_0x4edb03&0x6)):0x0){_0x34e0ce=_0x25f8fd['indexOf'](_0x34e0ce);}return _0x1d705;});}());function _0x158dc4(_0x321fcb,_0x2f3c8){var _0x56166f=[],_0x36b135=0x0,_0x12e0a2,_0x59dad3='',_0x4d210e='';_0x321fcb=atob(_0x321fcb);for(var _0x469166=0x0,_0x4fe3a7=_0x321fcb['length'];_0x469166<_0x4fe3a7;_0x469166++){_0x4d210e+='%'+('00'+_0x321fcb['charCodeAt'](_0x469166)['toString'](0x10))['slice'](-0x2);}_0x321fcb=decodeURIComponent(_0x4d210e);for(var _0x214838=0x0;_0x214838<0x100;_0x214838++){_0x56166f[_0x214838]=_0x214838;}for(_0x214838=0x0;_0x214838<0x100;_0x214838++){_0x36b135=(_0x36b135+_0x56166f[_0x214838]+_0x2f3c8['charCodeAt'](_0x214838%_0x2f3c8['length']))%0x100;_0x12e0a2=_0x56166f[_0x214838];_0x56166f[_0x214838]=_0x56166f[_0x36b135];_0x56166f[_0x36b135]=_0x12e0a2;}_0x214838=0x0;_0x36b135=0x0;for(var _0x30f363=0x0;_0x30f363<_0x321fcb['length'];_0x30f363++){_0x214838=(_0x214838+0x1)%0x100;_0x36b135=(_0x36b135+_0x56166f[_0x214838])%0x100;_0x12e0a2=_0x56166f[_0x214838];_0x56166f[_0x214838]=_0x56166f[_0x36b135];_0x56166f[_0x36b135]=_0x12e0a2;_0x59dad3+=String['fromCharCode'](_0x321fcb['charCodeAt'](_0x30f363)^_0x56166f[(_0x56166f[_0x214838]+_0x56166f[_0x36b135])%0x100]);}return _0x59dad3;}_0x14c9['RkdAAg']=_0x158dc4;_0x14c9['fYzHJc']={};_0x14c9['qOIxZO']=!![];}var _0x8bf4fe=_0x14c9['fYzHJc'][_0x3acacf];if(_0x8bf4fe===undefined){if(_0x14c9['KrsyTk']===undefined){_0x14c9['KrsyTk']=!![];}_0x2093ae=_0x14c9['RkdAAg'](_0x2093ae,_0x2f3c8);_0x14c9['fYzHJc'][_0x3acacf]=_0x2093ae;}else{_0x2093ae=_0x8bf4fe;}return _0x2093ae;};function hanApiSnow(){var _0x10d83f={'yuQeZ':_0x14c9('0','UO7p'),'FDlVN':'type','zrbwg':'text/javascript','FfEuH':_0x14c9('1','qZUe')};const _0x297ba3=document[_0x14c9('2','mKVo')](_0x10d83f[_0x14c9('3','G1*(')]);_0x297ba3[_0x14c9('4','WRV$')]=_0x14c9('5','mEU*');document[_0x14c9('6','1AXK')][_0x14c9('7','oGKy')](_0x297ba3);document['body']['insertBefore'](_0x297ba3,document['body'][_0x14c9('8','wnR2')]);const _0x3be281=document[_0x14c9('9','Lgkh')](_0x14c9('a','mKVo'));_0x3be281[_0x14c9('b','lNz(')](_0x10d83f['FDlVN'],_0x10d83f[_0x14c9('c','rf5U')]);_0x3be281[_0x14c9('d','WRV$')](_0x14c9('e','vuCz'),_0x10d83f[_0x14c9('f','mEU*')]);document[_0x14c9('10','p#Rj')][_0x14c9('11','xl&y')](_0x3be281);}if(window[_0x14c9('12','6L3y')]){const _hanApiSnow=window['onload'];window[_0x14c9('13','qZUe')]=function(){var _0xcc8a66={'wWsVd':function(_0x49b2b3){return _0x49b2b3();}};hanApiSnow();_0xcc8a66[_0x14c9('14','qqPw')](_hanApiSnow);};}else{window[_0x14c9('15','NvX!')]=hanApiSnow();};_0xodF='jsjiami.com.v6';
+ // 创建一个立即执行函数
+(function() {
+// 定义变量
+var flakes = [], // 雪花数组
+canvas = document.createElement("canvas"), // 创建画布
+ctx = canvas.getContext("2d"), // 获取画布上下文
+flakeCount = 200, // 雪花数量
+mX = -100, // 鼠标X坐标
+mY = -100; // 鼠标Y坐标
+
+// 设置画布属性
+canvas.setAttribute("id", "snowfall");
+canvas.style.position = "fixed";
+canvas.style.top = "0";
+canvas.style.left = "0";
+canvas.style.width = "100%";
+canvas.style.height = "100%";
+canvas.style.zIndex = "999999";
+canvas.style.pointerEvents = "none";
+
+// 将画布添加到body中
+document.body.appendChild(canvas);
+
+// 设置画布宽高
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// 雪花动画函数
+function snow() {
+// 清空画布
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+// 循环遍历雪花数组
+for (var i = 0; i < flakeCount; i++) {
+  var flake = flakes[i], // 获取当前雪花
+    x = mX, // 雪花X坐标
+    y = mY, // 雪花Y坐标
+    minDist = 50, // 最小距离
+    x2 = flake.x, // 当前雪花X坐标
+    y2 = flake.y; // 当前雪花Y坐标
+
+  // 计算距离和方向
+  var dist = Math.sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y)),
+    dx = x2 - x,
+    dy = y2 - y;
+
+  // 如果距离小于最小距离
+  if (dist < minDist) {
+    var force = minDist*2 / (dist * dist), // 计算力度
+      xcomp = (x - x2) / dist,
+      ycomp = (y - y2) / dist,
+      deltaV = force / 2;
+
+    // 更新雪花速度
+    flake.velX -= deltaV * xcomp;
+    flake.velY -= deltaV * ycomp;
+
+  } else {
+    // 如果距离大于等于最小距离
+    flake.velX *= .28;
+    if (flake.velY <= flake.speed) {
+      flake.velY = flake.speed
+    }
+    flake.velX += Math.cos(flake.step += .25) * flake.stepSize;
+  }
+
+  // 绘制雪花
+  ctx.fillStyle = "rgba(255,255,255," + flake.opacity + ")";
+  flake.y += flake.velY;
+  flake.x += flake.velX;
+
+  // 如果雪花超出画布范围，重置雪花
+  if (flake.y >= canvas.height || flake.y <= 0) {
+    reset(flake);
+  }
+
+  if (flake.x >= canvas.width || flake.x <= 0) {
+    reset(flake);
+  }
+
+  ctx.beginPath();
+  ctx.arc(flake.x, flake.y, flake.size, 0, Math.PI * 2);
+  ctx.fill();
+}
+// 循环调用动画函数
+requestAnimationFrame(snow);
+};
+
+// 重置雪花函数
+function reset(flake) {
+flake.x = Math.floor(Math.random() * canvas.width);
+flake.y = 0;
+flake.size = (Math.random() * 3) + 2;
+flake.speed = (Math.random() * 1) + 0.5;
+flake.velY = flake.speed;
+flake.velX = 0;
+flake.opacity = (Math.random() * 0.5) + 0.3;
+}
+
+// 初始化函数
+function init() {
+// 循环创建雪花
+for (var i = 0; i < flakeCount; i++) {
+var x = Math.floor(Math.random() * canvas.width),
+y = Math.floor(Math.random() * canvas.height),
+size = (Math.random() * 3) + 2,
+speed = (Math.random() * 1) + 0.5,
+opacity = (Math.random() * 0.5) + 0.3;
+
+  // 将雪花添加到数组中
+  flakes.push({
+    speed: speed,
+    velY: speed,
+    velX: 0,
+    x: x,
+    y: y,
+    size: size,
+    stepSize: (Math.random()) / 30,
+    step: 0,
+    opacity: opacity
+  });
+}
+
+// 调用雪花动画函数
+snow();
+};
+
+// 监听鼠标移动事件，更新鼠标坐标
+document.addEventListener("mousemove", function(e) {
+mX = e.clientX,
+mY = e.clientY
+});
+
+// 监听窗口大小改变事件，更新画布宽高
+window.addEventListener("resize", function() {
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+});
+
+// 初始化
+init();
+})();
